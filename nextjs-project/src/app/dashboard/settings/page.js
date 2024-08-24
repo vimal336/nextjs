2985329364159a4397533922fca105bc83ebdd14
vimal336 <asvimal336@gmail.com>
2024-08-25 00:35:34 +0530

    app router in next js

--- nextjs-project/src/app/dashboard/settings/page.js
+++ nextjs-project/src/app/dashboard/settings/page.js
@@ -1,12 +1,20 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Settings() {
+  const router = useRouter();
+   
+  const back = () => {
+    router.push('/dashboard');
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-<h1>Settings Page</h1>
-       
+      <button onClick={back} className="bg-blue-500 rounded text-white font-bold py-2 px-2 px-4 ">
+        Back
+      </button>
+      <h1>Settings Page</h1>
     </main>
   );
 }
- 
--- nextjs-project/src/app/page.js
+++ nextjs-project/src/app/page.js
@@ -4,7 +4,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button className="bg-blue-500 rounded text-white font-bold py-2 px-2 px-4 ">Back</button>
       <h1>Next JS Project</h1>
+      <button>Back</button>
+      <Link className="text-blue-600" href="/">Home</Link>
       <Link className="text-blue-600" href="/dashboard">Dashboard</Link>
       <Link className="text-blue-600" href="/dashboard/settings">Settings</Link>
     </main>
