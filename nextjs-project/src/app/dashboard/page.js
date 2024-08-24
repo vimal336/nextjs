106581c7e63e4ab18ae5a0a15631fd71f1edb7bf
vimal336 <asvimal336@gmail.com>
2024-08-25 01:12:55 +0530

    app router in next js

--- nextjs-project/src/app/dashboard/page.js
+++ nextjs-project/src/app/dashboard/page.js
@@ -1,8 +1,23 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
+  const router = useRouter();
+
+  const back = () => {
+    router.push("/");
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button
+        onClick={back}
+        className="bg-blue-500 rounded text-white font-bold py-2 px-2 px-4 "
+      >
+        Back
+      </button>
       <h1>Dashboard Page</h1>
     </main>
   );
