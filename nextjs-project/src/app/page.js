5ec65c0c9cd05c2d8e38bca47fb18dd4f4894009
vimal336 <asvimal336@gmail.com>
2024-08-26 15:03:19 +0530

    next js routing

--- nextjs-project/src/app/page.js
+++ nextjs-project/src/app/page.js
@@ -20,7 +20,6 @@ export default function Home() {
         </Link>
       </div>
 
-      {/* Back Button */}
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => window.history.back()}
