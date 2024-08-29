c75553c040fd1b77db7910e942c5c8b150e378cd
vimal336 <asvimal336@gmail.com>
2024-08-29 11:40:06 +0530

    user management

--- user-management-with-redux-and-nextjs/src/app/page.js
+++ user-management-with-redux-and-nextjs/src/app/page.js
@@ -2,14 +2,34 @@ import Link from "next/link";
 
 
 export const metadata = {
-  title: "About page",
+  title: "Home page",
   description: "Generated for about page",
 };
 
-export default function About() {
+export default function Home() {
   return (
     <>
-     <h1>crud</h1>
+    <nav className="bg-gray-800 p-4">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/about">
+              <a className="text-white hover:text-gray-300">About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a className="text-white hover:text-gray-300">Contact</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/adduser">
+              <a className="text-white hover:text-gray-300">Add User</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <h1 className="text-2xl font-bold mt-4">User Management</h1>
     </>
   );
 }
