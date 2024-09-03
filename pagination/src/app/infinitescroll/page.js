fe57dfd80ea27b98c7c285e2529b199bd6dccbb2
vimal336 <asvimal336@gmail.com>
2024-09-03 18:49:55 +0530

    infinitescroll

--- pagination/src/app/infinitescroll/page.js
+++ pagination/src/app/infinitescroll/page.js
@@ -84,7 +84,7 @@ export default function InfiniteScroll() {
         <button
           onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1 || isFetching}
-          className="px-4 py-2 bg-blue-500 text-white rounded "
+          className="px-4 py-2 bg-blue-500 text-white  "
         >
           Previous
         </button>
