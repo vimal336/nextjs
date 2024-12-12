9a66fbc1f3dede9629f02b88adec79adce51557a
vimal336 <asvimal336@gmail.com>
2024-12-12 17:57:15 +0530

    resuable button

---
+++ pagination/src/app/button/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = ({ children, className, onClick }) => {
+  return (
+    <button
+      className={`bg-blue-500 text-white font-bold py-2 px-4 rounded ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ pagination/src/app/usebtn/page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from '../button/page';
+
+const usebtn = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl mb-4">Sign In</h1>
+      {/* First Button with default width */}
+      <Button className="w-32 mb-4">Sign In</Button>
+      
+      {/* Second Button with different width and style */}
+      <Button className="w-48 bg-green-500 hover:bg-green-700">Sign In with Google</Button>
+    </div>
+  );
+};
+
+export default usebtn;
